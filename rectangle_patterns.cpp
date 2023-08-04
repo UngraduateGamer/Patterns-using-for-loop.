0b2c15c77c9bb9c1b8022b5eb2fd163dd3948ebb
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-08-04 10:22:06 +0530

    Create rectangle_patterns.cpp

---
+++ rectangle_patterns.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int r=5,c=7;
+    for (int i;i<=r;i++)
+    {
+        for (int j=1;j<=c;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
